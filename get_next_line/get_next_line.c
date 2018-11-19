b1780351e580582116df7015dd3a15d196e173cb
owalid <othmane.el.ayadi@gmail.com>
2018-11-19 20:46:36 +0100

    encore recommencer..

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,31 +6,43 @@
 /*   By: oel-ayad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 18:36:59 by oel-ayad          #+#    #+#             */
-/*   Updated: 2018/11/17 20:32:11 by oel-ayad         ###   ########.fr       */
+/*   Updated: 2018/11/19 20:44:51 by oel-ayad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
+#include <limits.h>
+void	ft_strjoinfree(char *s1, char **line)
+{
+	if (s1 || line)
+	{
+	*line = s1;
+	}
+}
 
 int		get_next_line(const int fd, char **line)
 {
-	static char		*gnl[];
-	char			buff[BUFF_SIZE + 1];
+	static char		*gnl[10240];
+	char			buff[BUFF_SIZE + 1]; //buffer fic
 
-	if (!line || BUFF_SIZE < 1 || read(fd, buff, BUFF_SIZE) < 0 || !gnl)
+	if (!line || BUFF_SIZE < 1 || read(fd, buff, BUFF_SIZE) < 0 || !(gnl[fd] = ft_strnew(0)))
 		return (-1);
-	while (!ft_strrchr(gnl[fd], '\n') && read(fd, buff, BUFF_SIZE))
+	// tant qu'on est pas sur un \n ni a la fin du fichier
+	while (read(fd, buff, BUFF_SIZE) != 0)
 	{
-		//tant qu'on est pas sur un \n ni a la fin du fichier
-		//.......
+		gnl[fd] = buff;
+		ft_strjoinfree(buff, line);
 	}
+	printf("%s", *line);
+	return (1);
 }
 
 int		main(int argc, char **argv)
 {
 	int 	fd;
-	char 	**line;
+	char 	*line; //la line
 
 	if (argc != 2)
 	{
@@ -41,7 +53,7 @@ int		main(int argc, char **argv)
 		return (1);
 	}
 	fd = open(argv[1], O_RDONLY);
-	get_next_line(fd, line);
+	get_next_line(fd, &line);
 	close(fd);
 	return (0);
 }
