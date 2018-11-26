b4c38dd3ee120ebeb24835b70e67fbd93ec9b8a9
owalid <othmane.el.ayadi@gmail.com>
2018-11-26 05:25:09 +0100

    corr de la libft

--- libft/ft_strnequ.c
+++ libft/ft_strnequ.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdlib.h>
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	size_t	i;
@@ -21,10 +21,10 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 		return (1);
 	if (!s1 || !s2)
 		return (0);
-	if (ft_strlen(s1) != ft_strlen(s2))
-		return (0);
 	while (i < n && (s1[i] || s2[i]))
 	{
+		printf(" s1: %c", s1[i]);
+		printf(" s2: %c\n", s2[i]);
 		if (s1[i] != s2[i])
 			return (0);
 		i++;
