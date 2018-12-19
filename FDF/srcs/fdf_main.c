89338842addb7e36917520ce4beb53d0f554b273
owalid <othmane.el.ayadi@gmail.com>
2018-12-19 23:23:02 +0100

    amelioraiton du main

--- FDF/srcs/fdf_main.c
+++ FDF/srcs/fdf_main.c
@@ -9,3 +9,23 @@ void	fdf_verif_ext(char *file)
 		fdf_err(2);
 }
 
+int 	main(int ac char **av)
+{
+	if (ac > 1 && ac < 5)
+	{
+		fdf_verif_ext(av[1]);
+		if (ac == 4)
+			// modifier les valeurs par default de la fenetre
+		else
+		{
+			info->width = 1280;
+			info->heigth = 720;
+		}
+		map = get_map(av[1]);
+		ft_parse(map, info);
+		ft_strdel(map);
+		ft_stctdel(info);
+	}
+	ft_err(0);
+	return (0);
+}
